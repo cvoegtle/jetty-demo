d956f705293fae1a1ccd4d6bb1e33ac8b687d185
Christian Vögtle <christian.voegtle@sn-invent.de>
2023-03-20 13:19:28 +0100

    Initial commit

---
+++ src/main/java/de/sninvent/jettydemo/client/Demo.java
@@ -0,0 +1,9 @@
+package de.sninvent.jettydemo.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class Demo implements EntryPoint {
+  @Override public void onModuleLoad() {
+
+  }
+}
